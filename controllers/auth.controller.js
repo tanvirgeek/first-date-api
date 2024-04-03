7356e05725f3b1b87d97e460d0e7a3606c2d66aa
tanvirgeek <tanvirgeek@gmail.com>
2024-04-03 09:12:38 +0600

    image upload

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import multer from 'multer';
 
 export const signup = async (req, res) => {
     try {
@@ -29,7 +30,6 @@ export const signup = async (req, res) => {
             birthDate,
             shortStory,
             profileImage,
-            threeImages,
             password,
             email
         } = req.body;
@@ -38,40 +38,48 @@ export const signup = async (req, res) => {
         if (user) {
             res.status(400).json({ error: "Email already exists!" })
         } else {
-            const newUser = new User({
-                fullname,
-                interestedIn,
-                attachmentStyle,
-                gender,
-                country,
-                isStudent,
-                studyTopic,
-                profession,
-                doesHaveApurpose,
-                purpose,
-                doYouReadBooks,
-                books,
-                musicians,
-                favoriteFoods,
-                favoriteMovies,
-                favoriteDrinks,
-                favoriteSportsTeam,
-                doesLikeTravelling,
-                doesHavePet,
-                aboutPet,
-                languages,
-                partnersAgeRangeMax,
-                partnersAgeRangeMin,
-                birthDate,
-                shortStory,
-                profileImage,
-                threeImages,
-                password,
-                email
-            });
+            if (req.files) {
+                // If the images are successfully uploaded, you can save their paths to the database or perform any other action here
+                // @ts-ignore
+                const imagePaths = req.files.map((file) => file.path);
+                const newUser = new User({
+                    fullname,
+                    interestedIn,
+                    attachmentStyle,
+                    gender,
+                    country,
+                    isStudent,
+                    studyTopic,
+                    profession,
+                    doesHaveApurpose,
+                    purpose,
+                    doYouReadBooks,
+                    books,
+                    musicians,
+                    favoriteFoods,
+                    favoriteMovies,
+                    favoriteDrinks,
+                    favoriteSportsTeam,
+                    doesLikeTravelling,
+                    doesHavePet,
+                    aboutPet,
+                    languages,
+                    partnersAgeRangeMax,
+                    partnersAgeRangeMin,
+                    birthDate,
+                    shortStory,
+                    profileImage: imagePaths[0],
+                    password,
+                    email,
+                    threeImages: imagePaths
+                });
 
-            const savedUser = await newUser.save()
-            res.status(201).json(savedUser)
+                const savedUser = await newUser.save()
+                res.status(201).json(savedUser)
+            } else {
+                res.status(400).json({ message: 'No images uploaded' });
+            }
+
         }
 
     } catch (error) {
@@ -86,3 +94,22 @@ export const signin = (req, res) => {
 export const signout = (req, res) => {
     res.send("sign out users")
 }
+
+// Set up multer storage
+export const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname);
+    },
+});
+
+// Filter for image files
+export const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true);
+    } else {
+        cb(new Error('Only image files are allowed!'), false);
+    }
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { signin, signout, signup } from "../controllers/auth.controller.js";
+import { fileFilter, signin, signout, signup, storage } from "../controllers/auth.controller.js";
+import multer from 'multer';
 
 const router = express.Router()
 
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
 router.post("/login", signin)
 
-router.post("/register", signup)
+router.post("/register", upload.array('images', 3), signup)
 
 router.post("/logout", signout)
 

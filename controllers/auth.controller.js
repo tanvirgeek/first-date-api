3a90c61f718e2440273b57f3252d934ec535c3ed
tanvirgeek <tanvirgeek@gmail.com>
2024-04-05 00:14:56 +0600

    user without password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -132,8 +132,9 @@ export const signin = async (req, res) => {
         if (process.env.ACCESS_TOKEN_SECRET && process.env.REFRESH_TOKEN_SECRET) {
             const accessToken = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             const refreshToken = jwt.sign({ userId: user._id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
-            const { password, ...userWthoutPassword } = user
-            res.json({ userWthoutPassword, accessToken, refreshToken });
+            // @ts-ignore
+            user.password = undefined
+            res.json({ user, accessToken, refreshToken });
         }
     } catch (error) {
         console.log(error);
--- models/user.model.js
+++ models/user.model.js
@@ -113,7 +113,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: true,
     },
     email: {
         type: String,

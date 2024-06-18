ab375be2098d8d0cba3ec090dedc8fe7c64249af
tanvirgeek <tanvirgeek@gmail.com>
2024-06-18 06:58:15 +0600

    Country City World

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,7 +3,7 @@ import SessionUsers from "../models/usersSession.model.js";
 
 export const getPaginatedUsersFromCity = async (req, res) => {
     try {
-        const { city, sessionId } = req.query;
+        const { city, country, sessionId } = req.query;
 
         // Get excludedUserIds for current session
         let sessionUsers = await SessionUsers.findOne({ sessionId });
@@ -15,21 +15,54 @@ export const getPaginatedUsersFromCity = async (req, res) => {
         // Query for random users not in excludedUserIds
         const randomUsers = await User.aggregate([
             { $match: { city, _id: { $nin: sessionUsers.excludedUserIds } } },
-            { $sample: { size: 6 } }
+            { $sample: { size: 3 } }
         ]);
 
         if (randomUsers.length === 0) {
-            return res.status(404).json({ message: "No users found matching the criteria." });
+            const randomUsersCountry = await User.aggregate([
+                { $match: { country, _id: { $nin: sessionUsers.excludedUserIds } } },
+                { $sample: { size: 3 } }
+            ]);
+
+            if (randomUsersCountry.length === 0) {
+                const randomUsersWorld = await User.aggregate([
+                    { $match: { _id: { $nin: sessionUsers.excludedUserIds } } },
+                    { $sample: { size: 3 } }
+                ]);
+                if (randomUsersWorld.length == 0) {
+                    return res.status(404).json({ message: "No users found matching the criteria." });
+                } else { 
+                    // Extract user IDs from randomUsers
+                    const randomUserIds = randomUsersWorld.map(user => user._id);
+
+                    // Update sessionUsers to include current user IDs in excludedUserIds
+                    sessionUsers.excludedUserIds = [...sessionUsers.excludedUserIds, ...randomUserIds];
+                    await sessionUsers.save();
+
+                    res.json(randomUsersWorld);
+                }
+            } else { 
+                // Extract user IDs from randomUsers
+                const randomUserIds = randomUsersCountry.map(user => user._id);
+
+                // Update sessionUsers to include current user IDs in excludedUserIds
+                sessionUsers.excludedUserIds = [...sessionUsers.excludedUserIds, ...randomUserIds];
+                await sessionUsers.save();
+
+                res.json(randomUsersCountry);
+            }
+        } else { 
+            // Extract user IDs from randomUsers
+            const randomUserIds = randomUsers.map(user => user._id);
+
+            // Update sessionUsers to include current user IDs in excludedUserIds
+            sessionUsers.excludedUserIds = [...sessionUsers.excludedUserIds, ...randomUserIds];
+            await sessionUsers.save();
+
+            res.json(randomUsers);
         }
 
-        // Extract user IDs from randomUsers
-        const randomUserIds = randomUsers.map(user => user._id);
-
-        // Update sessionUsers to include current user IDs in excludedUserIds
-        sessionUsers.excludedUserIds = [...sessionUsers.excludedUserIds, ...randomUserIds];
-        await sessionUsers.save();
-
-        res.json(randomUsers);
+        
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: "Server Error" });

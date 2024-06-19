44024d6d23c302f33fe7e12fafbe7a3e21122688
tanvirgeek <tanvirgeek@gmail.com>
2024-06-19 11:34:53 +0600

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { updatePassword, updateUserIformation } from "../controllers/user.controller.js";
+import { updatePassword, updateUserIformation, updateUserProfilePic } from "../controllers/user.controller.js";
+import { fileFilter, storage } from "../controllers/auth.controller.js";
+
+import multer from "multer";
 
 const router = express.Router()
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
 router.put("/updatePassword", updatePassword)
 router.put("/updateUserInfo", updateUserIformation)
+router.put("/updateProfilePic", upload.single("profilePic"), updateUserProfilePic)
 
 export default router
